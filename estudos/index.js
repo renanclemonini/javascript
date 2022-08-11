a6a30745257483495e3a441dac98789b53604030
Renan Sotelo Clemonini <106163654+renanclemonini@users.noreply.github.com>
2022-08-11 11:03:26 -0300

    atualizações calculadora imc

--- estudos/index.js
+++ estudos/index.js
@@ -1,7 +1,22 @@
 function calculate(){
-    var altura = document.getElementById('altura').value
+    var altura = (document.getElementById('altura').value)/100
     var peso = document.getElementById('peso').value
 
-    console.log(altura)
-    console.log(peso)
+    var imc = (peso / altura ** 2).toFixed(2)
+    var text = ""
+
+    if(imc<18.5){
+        text=(`Você esta com Peso Baixo, IMC = ${imc}`)
+    }else if(imc>=18.5&&imc<=24.9){
+        text=(`Você esta com Peso Normal, IMC = ${imc}`)
+    }else if(imc>=25&&imc<=29.9){
+        text=(`Você esta com Sobrepeso, IMC = ${imc}`)
+    }else if(imc>=30&&imc<=34.9){
+        text=(`Você esta com Obesidade Grau 1, IMC = ${imc}`)
+    }else if(imc>=35&&imc<=39.9){
+        text=(`Você esta com Obesidade Grau 2, IMC = ${imc}`)
+    }else if(imc>=40){
+        text=(`Você esta com Obesidade Grau 3, IMC = ${imc}`)
+    }
+    document.getElementById('text_area').innerText=text
 }
