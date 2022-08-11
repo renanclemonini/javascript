c20effe6e74f8f40574c597e319c81792230fb92
Renan Sotelo Clemonini <106163654+renanclemonini@users.noreply.github.com>
2022-08-10 23:46:00 -0300

    aprendendo

---
+++ estudos/index.js
@@ -0,0 +1,7 @@
+function calculate(){
+    var altura = document.getElementById('altura').value
+    var peso = document.getElementById('peso').value
+
+    console.log(altura)
+    console.log(peso)
+}
