ac17c8e2b49def7d6699c087374d33484c7e06d1
Renan Sotelo Clemonini <106163654+renanclemonini@users.noreply.github.com>
2022-08-16 23:16:10 -0300

    atualização

--- aula12exec/script.js
+++ aula12exec/script.js
@@ -1,31 +1,35 @@
 function carregar(){
     var msg = document.getElementById('msg')
-    var img = document.getElementById('imagem')
     var sds = document.getElementById('sds')
     var data = new Date()
     var hora = data.getHours()
+    var img = document.createElement('img')
+    img.setAttribute('id','foto')
     
     msg.innerHTML = `Agora são ${hora} horas!`
     
-
     if (hora >= 0 && hora <= 5){
         // BOA MADRUGADA
-        img.src = 'imagens/fotomadrugada.png'
+        img.setAttribute('src','imagens/fotomadrugada.png')
+        foto.appendChild(img)
         document.body.style.background = '#272b40'
         sds.innerHTML = 'Boa Madrugada!'
     }else if(hora >= 6 && hora < 12){
         // BOM DIA!
-        img.src = 'imagens/fotomanha.png'
+        img.setAttribute('src','imagens/fotomanha.png')
+        foto.appendChild(img)
         document.body.style.background = '#afa39c'
         sds.innerHTML = 'Bom dia!'
     }else if (hora >= 12 && hora <= 18){
         // BOA TARDE
-        img.src = 'imagens/fototarde.png'
+        img.setAttribute('src','imagens/fototarde.png')
+        foto.appendChild(img)
         document.body.style.background = '#434d3b'
         sds.innerHTML = 'Boa tarde!'
     }else {
         // BOA NOITE
-        img.src = 'imagens/fotonoite.png'
+        img.setAttribute('src','imagens/fotonoite.png')
+        foto.appendChild(img)
         document.body.style.background = '#011e2c'
         sds.innerHTML = 'Boa noite!'
     }
