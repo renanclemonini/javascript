78549944c3d326a60df482b091bc27cf96dc5dd3
Renan Sotelo Clemonini <106163654+renanclemonini@users.noreply.github.com>
2022-08-17 19:27:31 -0300

    imc new update

--- imcnew/script.js
+++ imcnew/script.js
@@ -1,20 +1,20 @@
 function calc(){
-    var peso = document.getElementById('peso')
-    var altura = document.getElementById('altura')
-    var imc = peso / altura ** 2
+    var peso = document.getElementById('pes').value
+    var altura = (document.getElementById('alt').value)/100
+    var imc = (peso / altura ** 2).toFixed(2)
+    var txt = ""
 
-    if (peso.value.length == 0 || altura.value.length == 0){
+    if (peso.length == 0 || altura.length == 0){
         alert('[ERRO] Digite o valor no campo determinado')
-    } else{
-        alert('tudo certo prossiga')
-    }
-
-
-
-
-
-
-
+    } else {
+        res.style.padding = '10px'
+        res.innerHTML = `Seu IMC é igual a ${imc}`
+    } 
     
+    if (imc < 18.5){
+        txt = "Sua condição esta abaixo do peso ideal"
+    }
+    
+    document.getElementById('cond').innerHTML=txt
 
 }
