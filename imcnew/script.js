04e13830a856603ce0aea4c8de71ef7aa6e82221
Renan Sotelo Clemonini <106163654+renanclemonini@users.noreply.github.com>
2022-08-17 23:02:34 -0300

    update new imc

--- imcnew/script.js
+++ imcnew/script.js
@@ -5,16 +5,61 @@ function calc(){
     var txt = ""
 
     if (peso.length == 0 || altura.length == 0){
-        alert('[ERRO] Digite o valor no campo determinado')
+        res.style.paddingLeft = '25px'
+        res.style.maxWidth = '340px'
+        res.style.textDecoration = 'underline'
+        res.style.fontWeight = 'bold'
+        res.style.color = 'red'
+        res.style.paddingTop = '20px'
+        res.innerHTML = "[ERRO] Digite seu peso e altura!"
+    } else if (imc < 18.5){
+        res.style.paddingLeft = '25px'
+        res.style.maxWidth = '340px'
+        res.style.textDecoration = 'underline'
+        res.style.fontWeight = 'bold'
+        res.style.color = 'black'
+        res.style.paddingTop = '20px'
+        res.innerHTML = `Seu IMC é igual a ${imc} e sua condição esta abaixo do peso ideal.`
+    } else if (imc < 24.9){
+        res.style.paddingLeft = '25px'
+        res.style.maxWidth = '340px'
+        res.style.textDecoration = 'underline'
+        res.style.fontWeight = 'bold'
+        res.style.color = 'black'
+        res.style.paddingTop = '20px'
+        res.innerHTML = `Seu IMC é igual a ${imc} e sua condição esta com peso ideal.`
+    } else if (imc < 29.9){
+        res.style.paddingLeft = '25px'
+        res.style.maxWidth = '340px'
+        res.style.textDecoration = 'underline'
+        res.style.fontWeight = 'bold'
+        res.style.color = 'black'
+        res.style.paddingTop = '20px'
+        res.innerHTML = `Seu IMC é igual a ${imc} e sua condição esta acima do peso ideal - sobrepeso!`
+    } else if (imc < 34.99){
+        res.style.paddingLeft = '25px'
+        res.style.maxWidth = '340px'
+        res.style.textDecoration = 'underline'
+        res.style.fontWeight = 'bold'
+        res.style.color = 'black'
+        res.style.paddingTop = '20px'
+        res.innerHTML = `Seu IMC é igual a ${imc} e sua condição esta acima do peso ideal - obesidade grau 1!`
+    } else if (imc < 39.99){
+        res.style.paddingLeft = '25px'
+        res.style.maxWidth = '340px'
+        res.style.textDecoration = 'underline'
+        res.style.fontWeight = 'bold'
+        res.style.color = 'black'
+        res.style.paddingTop = '20px'
+        res.innerHTML = `Seu IMC é igual a ${imc} e sua condição esta acima do peso ideal - obesidade grau 2!"`
     } else {
-        res.style.padding = '10px'
-        res.innerHTML = `Seu IMC é igual a ${imc}`
-    } 
-    
-    if (imc < 18.5){
-        txt = "Sua condição esta abaixo do peso ideal"
+        res.style.paddingLeft = '25px'
+        res.style.maxWidth = '340px'
+        res.style.textDecoration = 'underline'
+        res.style.fontWeight = 'bold'
+        res.style.color = 'black'
+        res.style.paddingTop = '20px'
+        res.innerHTML = `Seu IMC é igual a ${imc} e sua condição esta acima do peso ideal - obesidade grau 3!"`
     }
-    
-    document.getElementById('cond').innerHTML=txt
-
+        
 }
