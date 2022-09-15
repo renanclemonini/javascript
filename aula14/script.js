06c9afd8e7ac0e011ec853958542c5347e41a51f
Renan Sotelo Clemonini <106163654+renanclemonini@users.noreply.github.com>
2022-09-15 18:31:17 -0300

    new

---
+++ aula14/script.js
@@ -0,0 +1,9 @@
+function contar(){
+    var begin = document.getElementById("inicio")
+    var end = document.getElementById("fim")
+    var passo = document.getElementById("pass")
+
+    var contando = begin + passo + end 
+
+    contagem.innerhtml = contando
+}
