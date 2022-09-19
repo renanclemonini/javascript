058c915ec2721bdb304ecfcd2b9d0ea93f3c7871
Renan Sotelo Clemonini <106163654+renanclemonini@users.noreply.github.com>
2022-09-18 22:35:15 -0300

    aprendendo
    fomos ate o tempo de 01:09:39

---
+++ urna2/etapas.js
@@ -0,0 +1,16 @@
+let etapas = [
+    {
+        titulo: 'Presidente',
+        numeros: 2,
+        candidatos: [
+            numero: 22,
+            nome: 'Bolsonaro',
+            Partido: 'PL',
+            Vice: 'Braga Neto',
+            fotos:[
+                {url: 'imagens/bolsonaro.png', leganda: 'Presidente'},
+                {url: 'imagens/braganeto.png', legenda: 'Vice-Presidente', small: true}
+            ]
+        ]
+    }
+]
