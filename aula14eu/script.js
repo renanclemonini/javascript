136e2aba993a16a97065be2ae39f4d838f406612
Renan Sotelo Clemonini <106163654+renanclemonini@users.noreply.github.com>
2022-09-22 14:00:34 -0300

    finish

--- aula14eu/script.js
+++ aula14eu/script.js
@@ -5,11 +5,26 @@ function contar(){
     let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        alert('[ERRO] Faltam dados!')
-    } else {
         res.style.fontWeight = 'bold'
-        res.innerHTML = 'Tudo certo!'
+        res.innerHTML = '[ERRO] Faltam dados'
+    } else {
+        res.innerHTML = 'Contando...'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0){
+            alert('Valor do passo inválido. Considerando passo 1')
+            p = 1
+        }
+        if (i < f){
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += ` ${c} \u{1f449} `
+            }
+        } else {
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += ` ${c} \u{1f449} `
+            }
+        }
+        res.innerHTML += `\u{1f3c1}`
     }
-
-
 }
