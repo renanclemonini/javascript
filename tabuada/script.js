9edbf58717a510e156171599a0dd2c9b6d38bf44
Renan Sotelo Clemonini <106163654+renanclemonini@users.noreply.github.com>
2022-09-22 14:58:12 -0300

    tabuada

---
+++ tabuada/script.js
@@ -0,0 +1,16 @@
+function tabuada(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0){
+        alert('Por favor digite um numero')
+    } else {
+        let n = Number(num.value)
+        tab.innerHTML = ''
+        for(c = 1; c <= 10; c++){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+        }
+    }
+}
+
