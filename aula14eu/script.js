0273f511c53e0930f22d4f8494ddd78ab067736b
Renan Sotelo Clemonini <106163654+renanclemonini@users.noreply.github.com>
2022-09-22 13:21:07 -0300

    new
    adaptando

---
+++ aula14eu/script.js
@@ -0,0 +1,15 @@
+function contar(){
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        alert('[ERRO] Faltam dados!')
+    } else {
+        res.style.fontWeight = 'bold'
+        res.innerHTML = 'Tudo certo!'
+    }
+
+
+}
