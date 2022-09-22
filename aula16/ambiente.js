13563ce2149f6c23dceeed697c793597d285b716
Renan Sotelo Clemonini <106163654+renanclemonini@users.noreply.github.com>
2022-09-22 18:29:45 -0300

    aula funções

---
+++ aula16/ambiente.js
@@ -0,0 +1,9 @@
+let num = 29
+
+if (num%2==0){
+    console.log(`O numero ${num} é par`)
+    return 'par'
+} else {
+    console.log(`O numero ${num} não é par`)
+    return 'impar'
+}
---
+++ aula16/function02.js
@@ -0,0 +1,6 @@
+function dividir(n1=0, n2=1){
+    return n1 / n2
+}
+
+console.log(`A divisão é igual a ${dividir(152365, 2)}`)
+
