4e5ec25115a9553637793e10f455db3d936417d5
Renan Sotelo Clemonini <106163654+renanclemonini@users.noreply.github.com>
2022-11-10 16:04:28 -0300

    Update script.js

--- urnaeletronica/script.js
+++ urnaeletronica/script.js
@@ -1,5 +1,7 @@
-let dig1 = document.getElementById('alg1')
-let dig2 = document.getElementById('alg2')
+let dig1 = document.getElementById('alg1');
+let dig2 = document.getElementById('alg2');
 
-let num1 = document.getElementById('num1')
-let num2 = document.getElementById('num2')
+let num1 = document.getElementById('num1');
+let num2 = document.getElementById('num2');
+
+let bolsonaro = 
